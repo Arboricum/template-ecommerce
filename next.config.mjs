9dd80ecef97e725f6aab2ebec15f8dd1f1d8bc4a
Arboricum <122838892+Arboricum@users.noreply.github.com>
2024-09-25 16:01:42 +0200

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'utfs.io',
+      },
+      {
+        protocol: 'https',
+        hostname: 'drive.google.com',
+      }
+    ],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,  // Disabilita ESLint durante la build
+  },
+};
 
 export default nextConfig;
+
